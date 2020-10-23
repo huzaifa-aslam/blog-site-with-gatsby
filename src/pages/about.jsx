03d6664be2859fedd591df58a5bee12d80da7ca8
huzaifa aslam <huzaifaslam45@gmail.com>
2020-10-23 15:10:15 +0500

    add text in about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -30,10 +30,11 @@ const About = () => {
         <Grid container spacing={3}>
 
           <Grid item xs={12} lg={8} md={8} sm={6}>
-            <Typography style={{textAlign:'center'}} gutterBottom variant="h5" component="h2">
-              Our Team
+            <Typography  gutterBottom variant="h6" component="h2">
+             
+             
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
             </Typography>
-
            
           </Grid>
 
